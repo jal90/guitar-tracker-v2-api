3a94000c65a5b822c66a86c2c0edb079e801afa9
jal90 <mezzyjezze@gmail.com>
2018-08-29 16:17:06 -0400

    Change relationship of Setup to User
    Setups now belongs_to User instead of has_many through
    In attempt to be able to query setup.user
    
    Currently need to go through user_guitar like setup.user_guitar.user
    which isn't a problem

--- app/models/setup.rb
+++ app/models/setup.rb
@@ -1,7 +1,6 @@
 class Setup < ApplicationRecord
   belongs_to :user_guitar
-  has_many :users, through: :user_guitars
-  # belongs_to :user
+  belongs_to :user
 
   default_scope { order(created_at: :desc) }
 
